0862bf8e3744b23484a557e51ec0888a309e7fc1
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-09-28 12:52:50 -0700

    fixes

--- src/main/database/database-conn.ts
+++ src/main/database/database-conn.ts
@@ -1,20 +1,18 @@
 /* eslint-disable no-use-before-define */
-import BaseDB from './base';
-import FakeDB from './fake-db';
 import SQLiteDB from './sqlite-db';
 
 export default class DatabaseConn {
-  private db: BaseDB;
+  private db: SQLiteDB;
 
   static #instance: DatabaseConn | null = null;
 
-  constructor(db: BaseDB) {
+  constructor(db: SQLiteDB) {
     this.db = db;
   }
 
-  static async getDatabase(env: 'fake' | 'prod'): Promise<BaseDB> {
+  static async getDatabase(): Promise<SQLiteDB> {
     if (!DatabaseConn.#instance) {
-      const db = env === 'fake' ? new FakeDB() : new SQLiteDB();
+      const db = new SQLiteDB();
       await db.connect();
       DatabaseConn.#instance = new DatabaseConn(db);
     }
