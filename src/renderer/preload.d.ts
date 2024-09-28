eba959dedfe4d6cf80244e4f4d02a5281db3c8fd
Shriram Giridhara <shrirambhavin@gmail.com>
2024-09-28 19:16:50 -0400

    Fix lint

--- src/renderer/preload.d.ts
+++ src/renderer/preload.d.ts
@@ -1,8 +1,4 @@
-import { RegistrationHandler } from '../main/preload';
-import {
-  JobHandler,
-  RegistrationHandler,
-} from '../main/preload';
+import { JobHandler, RegistrationHandler } from '../main/preload';
 
 declare global {
   // eslint-disable-next-line no-unused-vars
