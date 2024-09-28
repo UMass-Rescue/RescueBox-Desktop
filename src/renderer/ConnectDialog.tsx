205d25e2a8262f70ce01cece43865c4f2825f40f
Shreyan Mallik <shrnmallik@gmail.com>
2024-09-28 14:50:39 -0400

    finished nav bar & register page

---
+++ src/renderer/ConnectDialog.tsx
@@ -0,0 +1,45 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@shadcn/components/ui/dialog';
+import { Button } from './components/ui/button';
+import { Label } from './components/ui/label';
+import { Input } from './components/ui/input';
+
+function ConnectDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger>Connect</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Connect Model</DialogTitle>
+          <DialogDescription>
+            <h1>Model: Image Super Resolution</h1>
+            <span>Description: Super Resolution model for images</span>
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            <Input id="link" defaultValue="0.0.0.0" readOnly />
+          </div>
+          <DialogClose asChild>
+            <Button type="submit" size="sm" className="px-3">
+              Submit
+            </Button>
+          </DialogClose>
+        </div>
+        <DialogFooter className="sm:justify-start" />
+      </DialogContent>
+    </Dialog>
+  );
+}
+export default ConnectDialog;
