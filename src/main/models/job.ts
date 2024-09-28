5035b256e9bd890325b64fdbfb856dd32a964f2d
Shriram Giridhara <shrirambhavin@gmail.com>
2024-09-28 18:38:49 -0400

    Add job routes

--- src/main/models/job.ts
+++ src/main/models/job.ts
@@ -25,6 +25,94 @@ class Job extends Model {
   public parameters!: string; // JSON string
 
   public logOutput!: string; // JSON string
+
+  public static getAllJobs() {
+    return Job.findAll();
+  }
+
+  public static getJobByStatus(status: JobStatus) {
+    return Job.findAll({
+      where: {
+        status,
+      },
+    });
+  }
+
+  public static getJobByUid(uid: string) {
+    return Job.findOne({
+      where: {
+        uid,
+      },
+    });
+  }
+
+  public static createJob(
+    uid: string,
+    modelUid: string,
+    startTime: Date,
+    inputDir: string,
+    outputDir: string,
+    parameters: string,
+    logOutput: string,
+  ) {
+    return Job.create({
+      uid,
+      modelUid,
+      startTime,
+      inputDir,
+      outputDir,
+      parameters,
+      logOutput,
+      status: JobStatus.Running,
+    });
+  }
+
+  public static updateJobStatus(uid: string, status: JobStatus) {
+    return Job.update(
+      {
+        status,
+      },
+      {
+        where: {
+          uid,
+        },
+      },
+    );
+  }
+
+  public static updateJobEndTime(uid: string, endTime: Date) {
+    return Job.update(
+      {
+        endTime,
+      },
+      {
+        where: {
+          uid,
+        },
+      },
+    );
+  }
+
+  public static updateJobLogOutput(uid: string, logOutput: string) {
+    return Job.update(
+      {
+        logOutput,
+      },
+      {
+        where: {
+          uid,
+        },
+      },
+    );
+  }
+
+  public static deleteJob(uid: string) {
+    return Job.destroy({
+      where: {
+        uid,
+      },
+    });
+  }
 }
 
 export const initJob = async (connection: Sequelize) => {
