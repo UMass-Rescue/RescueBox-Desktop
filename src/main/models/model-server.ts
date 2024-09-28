a0063f03b798781cd44106d4efde67b1a1f738bb
Shriram Giridhara <shrirambhavin@gmail.com>
2024-09-28 18:39:24 -0400

    Add model-server queries

--- src/main/models/model-server.ts
+++ src/main/models/model-server.ts
@@ -8,6 +8,69 @@ class ModelServer extends Model {
   public serverAddress?: string;
 
   public serverPort?: number;
+
+  public static getAllServers() {
+    return ModelServer.findAll();
+  }
+
+  public static getServerByModelUid(modelUid: string) {
+    return ModelServer.findOne({
+      where: {
+        modelUid,
+      },
+    });
+  }
+
+  public static registerServer(
+    uid: string,
+    modelUid: string,
+    serverAddress: string,
+    serverPort: number,
+  ) {
+    return ModelServer.create({
+      uid,
+      modelUid,
+      serverAddress,
+      serverPort,
+    });
+  }
+
+  public static updateServer(
+    uid: string,
+    modelUid: string,
+    serverAddress: string,
+    serverPort: number,
+  ) {
+    return ModelServer.update(
+      {
+        serverAddress,
+        serverPort,
+      },
+      {
+        where: {
+          uid,
+          modelUid,
+        },
+      },
+    );
+  }
+
+  public static deleteServer(uid: string, modelUid: string) {
+    return ModelServer.destroy({
+      where: {
+        uid,
+        modelUid,
+      },
+    });
+  }
+
+  public static deleteServerByUid(uid: string) {
+    return ModelServer.destroy({
+      where: {
+        uid,
+      },
+    });
+  }
 }
 
 export const initModelServer = async (connection: Sequelize) => {
