9bd5f6d306531d3d4ec8090334a2f42acb325a8d
Shriram Giridhara <shrirambhavin@gmail.com>
2024-09-29 10:07:50 -0400

    Style the models page

--- src/renderer/Models.tsx
+++ src/renderer/Models.tsx
@@ -1,6 +1,13 @@
 import { Link } from 'react-router-dom';
 import sampleModels from './sample_models.json';
-import { Table, TableBody, TableCell, TableRow } from './components/ui/table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './components/ui/table';
 import { Model } from './Types';
 
 function Models() {
@@ -9,32 +16,54 @@ function Models() {
       <h1 className="font-bold text-xl md:text-2xl lg:text-4xl mb-4">
         Available Models
       </h1>
-      <Table className="mt-2 text-md lg:text-lg">
-        <TableBody>
-          {sampleModels.map((model: Model) => (
-            <TableRow>
-              <TableCell className="w-[70%]">{model.name}</TableCell>
-              <TableCell className="text-right">
-                <Link
-                  to={`/models/${model.uid}/details`}
-                  className="bg-inherit text-black border border-black font-semibold hover:bg-slate-300 p-2 rounded-lg"
-                >
-                  Inspect
-                </Link>
-              </TableCell>
-              <TableCell className="text-right">
-                <Link
-                  to="/model-run"
-                  className="bg-inherit text-black border border-black font-semibold hover:bg-slate-300 p-2 rounded-lg"
-                >
-                  Run
-                </Link>
-              </TableCell>
-              <TableCell className="text-right">{model.status}</TableCell>
+      <div className="shadow-md mt-2">
+        <Table className="mt-2 text-md lg:text-lg">
+          <TableHeader className="bg-slate-200">
+            <TableRow className="justify-between">
+              <TableHead className="pl-4 w-3/6 text-gray-900">Model</TableHead>
+              <TableHead className="w-1/6 text-gray-900">Status</TableHead>
+              <TableHead className="w-1/12" />
+              <TableHead className="w-1/12" />
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {sampleModels.map((model: Model) => (
+              <TableRow>
+                <TableCell className="pl-4">{model.name}</TableCell>
+                <TableCell className="">
+                  <div className="">
+                    <h1
+                      className={`w-min py-1 px-2 rounded-lg text-right transition-all cursor-default ${
+                        model.status === 'Online'
+                          ? 'bg-green-500 hover:bg-green-400'
+                          : 'bg-red-500 hover:bg-red-400'
+                      }`}
+                    >
+                      {model.status}
+                    </h1>
+                  </div>
+                </TableCell>
+                <TableCell className="text-left">
+                  <Link
+                    to={`/models/${model.uid}/details`}
+                    className="bg-slate-300 hover:-translate-y-0.5 hover:bg-slate-200 transition-all py-1 px-2 rounded-lg"
+                  >
+                    Inspect
+                  </Link>
+                </TableCell>
+                <TableCell className="text-left">
+                  <Link
+                    to="/model-run"
+                    className="bg-slate-300 hover:-translate-y-0.5 hover:bg-slate-200 transition-all py-1 px-2 rounded-lg"
+                  >
+                    Run
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
