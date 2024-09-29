25d1d15846e00b704e71ccd1e38a57fe726b120c
Atharva Kale <atharvakale343@gmail.com>
2024-09-29 11:36:23 -0400

    add a custom title bar

--- src/main/main.ts
+++ src/main/main.ts
@@ -97,6 +97,8 @@ const createWindow = async () => {
   };
 
   mainWindow = new BrowserWindow({
+    titleBarStyle: 'hiddenInset', // or 'customButtonsOnHover'
+    backgroundColor: '#333',
     show: false,
     width: 1024,
     height: 728,
