6a4bcfa676c320ca50c1b973893632532d798344
Shriram Giridhara <shrirambhavin@gmail.com>
2024-10-05 17:45:10 -0400

    Validate input for registration

--- src/renderer/ConnectDialog.tsx
+++ src/renderer/ConnectDialog.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -20,6 +19,8 @@ import { Button } from './components/ui/button';
 import { Label } from './components/ui/label';
 import { Input } from './components/ui/input';
 import { ConnectIcon } from './components/ConnectIcon';
+import { useMLModel } from './lib/hooks';
+import LoadingIcon from './components/LoadingIcon';
 
 type ConnectInputs = {
   ipAndPort: string;
@@ -32,17 +33,26 @@ function ConnectDialog({
 }: {
   defaultValue: string;
   modelUid: string;
-  registerModel: (modelUid: string, ipAndPort: string) => void;
+  registerModel: (modelUid: string, ipAndPort: string) => any;
 }) {
   // Form Hook
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<ConnectInputs>();
-  const onSubmit: SubmitHandler<ConnectInputs> = (data) => {
-    registerModel(modelUid, data.ipAndPort);
+    formState: { errors, isValid },
+  } = useForm<ConnectInputs>({ mode: 'onChange' });
+  const {
+    data: model,
+    error: modelError,
+    isLoading: modelIsLoading,
+  } = useMLModel(modelUid);
+
+  const onSubmit: SubmitHandler<ConnectInputs> = async (data) => {
+    await registerModel(modelUid, data.ipAndPort);
   };
+
+  if (modelIsLoading) return <LoadingIcon />;
+  if (modelError) return <p>Error: {modelError.message}</p>;
 
   return (
     <Dialog>
@@ -68,9 +78,7 @@ function ConnectDialog({
         <DialogHeader>
           <DialogTitle>Connect Model</DialogTitle>
           <DialogDescription>
-            Model: Image Super Resolution
-            <br />
-            Description: Super Resolution model for images
+            {model!.name} - Version {model!.version}
           </DialogDescription>
         </DialogHeader>
         <form
@@ -78,21 +86,30 @@ function ConnectDialog({
           className="flex items-center space-x-2"
         >
           <div className="grid flex-1 gap-2">
-            <Label htmlFor="link" className="sr-only">
-              Link
+            <Label htmlFor="ipAndPort" className="text-sm font-medium">
+              Server Address (IP:Port)
             </Label>
             <Input
-              {...register('ipAndPort', { required: true })}
+              {...register('ipAndPort', {
+                required: 'This field is required',
+                pattern: {
+                  value:
+                    /^(localhost|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)):(6553[0-5]|655[0-2][0-9]|65[0-4][0-9][0-9]|6[0-4][0-9][0-9][0-9][0-9]|[1-5](\d){4}|[1-9](\d){0,3})$/,
+                  message: 'Invalid IP:Port format',
+                },
+              })}
               defaultValue={`${defaultValue}`}
             />
-            {errors.ipAndPort && <span>This field is required</span>}
           </div>
-          <DialogClose asChild>
-            <Button type="submit" size="sm" className="px-3">
-              Connect
-            </Button>
-          </DialogClose>
+          <Button size="default" className="px-3 self-end" disabled={!isValid}>
+            Connect
+          </Button>
         </form>
+        {errors.ipAndPort && (
+          <span className="text-red-500 text-xs -mt-3">
+            Please enter a valid server address.
+          </span>
+        )}
         <DialogFooter className="sm:justify-start" />
       </DialogContent>
     </Dialog>
