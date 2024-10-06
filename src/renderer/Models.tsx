dda0b693af20ead7a2ee1aae85d3e400150d2432
Shriram Giridhara <shrirambhavin@gmail.com>
2024-10-06 16:21:10 -0400

    Refactor models tables

--- src/renderer/Models.tsx
+++ src/renderer/Models.tsx
@@ -1,26 +1,10 @@
 import { Link } from 'react-router-dom';
-import {
-  TooltipProvider,
-  Tooltip,
-  TooltipTrigger,
-  TooltipContent,
-} from '@shadcn/components/ui/tooltip';
-import { MLModel, ModelAppStatus } from 'src/shared/models';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from './components/ui/table';
 import { Button } from './components/ui/button';
-import { GreenCircleIcon, RedCircleIcon } from './components/CircleIcons';
-import GreenRunIcon from './components/GreenRunIcon';
 import { ConnectIcon } from './components/ConnectIcon';
 import { useMLModels, useServers, useServerStatuses } from './lib/hooks';
 import LoadingIcon from './components/LoadingIcon';
 import LoadingScreen from './components/LoadingScreen';
+import ModelsTable from './ModelsTable';
 
 function Models() {
   // ML Models Hook
@@ -57,6 +41,10 @@ function Models() {
     (model) => serverStatuses[model.uid] === 'Online',
   );
 
+  const offModels = models.filter(
+    (model) => serverStatuses[model.uid] !== 'Online',
+  );
+
   if (onModels.length === 0) {
     return (
       <div className="m-3 flex flex-col gap-8">
@@ -88,87 +76,17 @@ function Models() {
         Available Models
         {statusIsValidating && <LoadingIcon className="size-8 text-blue-600" />}
       </h1>
-      <div className="shadow-md mt-6 max-w-[900px]">
-        <Table className="text-md lg:text-lg">
-          <TableHeader className="bg-slate-200">
-            <TableRow className="justify-between">
-              <TableHead className="pl-4 w-3/6 text-gray-900">Model</TableHead>
-              <TableHead className="w-1/6 text-gray-900">Status</TableHead>
-              <TableHead className="w-1/12" />
-              <TableHead className="w-1/12" />
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {onModels
-              .filter(
-                (model: MLModel) =>
-                  serverStatuses[model.uid] === ModelAppStatus.Online,
-              )
-              .map((model: MLModel) => (
-                <TableRow key={model.uid} className="hover:bg-gray-50">
-                  <TableCell className="pl-4">{model.name}</TableCell>
-                  <TableCell className="">
-                    <div className="pl-4">
-                      {serverStatuses[model.uid] === 'Online' ? (
-                        <GreenCircleIcon />
-                      ) : (
-                        <RedCircleIcon />
-                      )}
-                    </div>
-                  </TableCell>
-                  <TableCell className="text-left">
-                    <Link
-                      to={`/models/${model.uid}/details`}
-                      className="py-2 px-5 "
-                    >
-                      <Button
-                        variant="outline"
-                        className="hover:-translate-y-0.5 transition-all  rounded-lg"
-                      >
-                        Inspect
-                      </Button>
-                    </Link>
-                  </TableCell>
-                  <TableCell className="text-left">
-                    <TooltipProvider delayDuration={100}>
-                      {serverStatuses[model.uid] === 'Online' && ( //
-                        <Tooltip>
-                          <TooltipTrigger asChild>
-                            <Link to={`/models/${model.uid}/run`}>
-                              <Button className="w-full  hover:-translate-y-0.5 transition-all py-2 px-6 rounded-lg bg-green-600 hover:bg-green-500">
-                                <GreenRunIcon />
-                              </Button>
-                            </Link>
-                          </TooltipTrigger>
-                          <TooltipContent side="bottom">
-                            <p>Run</p>
-                          </TooltipContent>
-                        </Tooltip>
-                      )}
-                      {serverStatuses[model.uid] === 'Offline' && (
-                        <Tooltip>
-                          <TooltipTrigger asChild>
-                            <Link to="/registration">
-                              <Button
-                                variant="outline"
-                                className=" text-black text-base w-full font-normal hover:-translate-y-0.5 transition-all py-2 px-6 rounded-lg"
-                              >
-                                <ConnectIcon />
-                              </Button>
-                            </Link>
-                          </TooltipTrigger>
-                          <TooltipContent side="bottom">
-                            <p>Connect</p>
-                          </TooltipContent>
-                        </Tooltip>
-                      )}
-                    </TooltipProvider>
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </div>
+      {onModels.length > 0 && (
+        <ModelsTable models={onModels} serverStatuses={serverStatuses} />
+      )}
+      <br className="my-5" />
+      <h1 className="font-bold text-xl md:text-2xl lg:text-4xl mb-4 flex flex-row gap-8 items-center">
+        Unavailable Models
+        {statusIsValidating && <LoadingIcon className="size-8 text-blue-600" />}
+      </h1>
+      {offModels.length > 0 && (
+        <ModelsTable models={offModels} serverStatuses={serverStatuses} />
+      )}
     </div>
   );
 }
