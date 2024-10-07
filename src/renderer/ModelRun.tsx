b95cc12ad88623256c9b0348241e754e9d902bc4
Shreyan Mallik <shrnmallik@gmail.com>
2024-10-06 22:06:05 -0400

    added sample pages for isr and sbf models

--- src/renderer/ModelRun.tsx
+++ src/renderer/ModelRun.tsx
@@ -1,8 +1,12 @@
 import { useParams } from 'react-router-dom';
-import RunImageObjDect from './model-run-pages/run-image-obj-dect';
+import RunImageObjDect from './model-run-pages/img-obj-dect-model/run-image-obj-dect';
+import RunISR from './model-run-pages/isr-model/run-isr';
+import RunSBF from './model-run-pages/sbf-model/run-sbf';
 
 const currentModels = {
   'obj-detection-model': RunImageObjDect,
+  'isr-model': RunISR,
+  'sbf-model': RunSBF,
 };
 
 function ModelRun() {
