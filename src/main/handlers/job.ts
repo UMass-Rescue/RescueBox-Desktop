560f6af7f0b6457d2b0f2f8a59f32b68fa85550e
Atharva Kale <atharvakale343@gmail.com>
2024-10-06 22:45:11 -0400

    add logging

--- src/main/handlers/job.ts
+++ src/main/handlers/job.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { Inputs, Outputs, Parameters } from 'src/shared/job';
-import { error, log } from 'electron-log/main';
+import log from 'electron-log/main';
 import JobDb, { JobStatus } from '../models/job';
 import { getServiceByModelUid } from '../model-apps/config';
 import ModelServerDb from '../models/model-server';
@@ -32,10 +32,15 @@ export type CompleteJobArgs = {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getJobs = async (_event: any, _arg: any) => {
+  log.info('Getting all jobs from the database');
   return JobDb.getAllJobs().then((jobsDb) => jobsDb.map(getRaw));
 };
 
 const completeJob = async (args: CompleteJobArgs) => {
+  log.info(
+    'Updating job end time and status in the database for job',
+    args.uid,
+  );
   await JobDb.updateJobEndTime(args.uid, args.endTime);
   await JobDb.updateJobStatus(args.uid, args.status);
   if (args.status === JobStatus.Failed) {
@@ -46,6 +51,7 @@ const completeJob = async (args: CompleteJobArgs) => {
 };
 
 const cancelJob = async (_event: any, args: JobByIdArgs) => {
+  log.info('Canceling job', args.uid);
   const manager = new InferenceTask(
     await JobDb.getJobByUid(args.uid).then((job) =>
       getServiceByModelUid(job!.modelUid),
@@ -63,12 +69,12 @@ const runJob = async (_event: any, arg: RunJobArgs) => {
   const uid = uuidv4();
   const manager = new InferenceTask(getServiceByModelUid(arg.modelUid));
   const server = await ModelServerDb.getServerByModelUid(arg.modelUid);
-  log(`Getting server for model ${arg.modelUid}`);
+  log.info(`Getting server for model ${arg.modelUid}`);
   if (!server) {
     throw new Error(`Server not found for model ${arg.modelUid}`);
   }
 
-  log('Trying to create a job in the Job model');
+  log.info('Trying to create a job in the Job model');
 
   // Create a job in the database
   try {
@@ -81,26 +87,26 @@ const runJob = async (_event: any, arg: RunJobArgs) => {
       arg.parameters,
     );
   } catch (err) {
-    error(
+    log.error(
       `Encountered an error while creating job for model id ${arg.modelUid}`,
     );
-    error('Error details:', err);
+    log.error('Error details:', err);
     throw new Error('Error creating job');
   }
 
   // Call the inference service for the particular model,
   // and update the job status after a reply is received
-  log('Calling inference service');
+  log.info('Calling inference service');
   manager
     .runInference({ ...arg, server })
     .then(async (response: SuccessResponse | ErrorResponse) => {
       const job = await JobDb.getJobByUid(uid);
       if (job?.status === JobStatus.Canceled) {
-        log('Job Canceled: Not updating job information.');
+        log.info('Job Canceled: Not updating job information.');
         return null;
       }
       if (response instanceof SuccessResponse) {
-        log('SuccessResponse: Updating job information.');
+        log.info('SuccessResponse: Updating job information.');
         completeJob({
           uid,
           endTime: new Date(),
@@ -110,7 +116,7 @@ const runJob = async (_event: any, arg: RunJobArgs) => {
         return null;
       }
       if (response instanceof ErrorResponse) {
-        log('ErrorResponse: Updating job information.');
+        log.info('ErrorResponse: Updating job information.');
         completeJob({
           uid,
           endTime: new Date(),
@@ -122,7 +128,7 @@ const runJob = async (_event: any, arg: RunJobArgs) => {
       throw new Error('FATAL: Invalid response type.');
     })
     .catch(async (err) => {
-      log('Request failed: Updating job information.');
+      log.error('Request failed: Updating job information.');
       completeJob({
         uid,
         status: JobStatus.Failed,
@@ -131,15 +137,17 @@ const runJob = async (_event: any, arg: RunJobArgs) => {
       } as CompleteJobArgs);
       return null;
     });
-  log('Job model created successfully, fetching job from database.');
+  log.info('Job model created successfully, fetching job from database.');
   return JobDb.getJobByUid(uid).then(getRaw);
 };
 
 const getJobById = async (_event: any, arg: JobByIdArgs) => {
+  log.info('Getting job by id', arg.uid);
   return JobDb.getJobByUid(arg.uid).then(getRaw);
 };
 
 const deleteJobById = async (_event: any, arg: JobByIdArgs) => {
+  log.info('Deleting job by id', arg.uid);
   return JobDb.deleteJob(arg.uid);
 };
 
--- src/main/handlers/logging.ts
+++ src/main/handlers/logging.ts
@@ -2,9 +2,11 @@
 import log from 'electron-log/main';
 
 export async function getLogs(_event: any, _arg: any) {
+  log.info('Getting logs');
   return log.transports.file.readAllLogs();
 }
 
 export async function clearLogs(_event: any, _arg: any) {
+  log.info('Clearing logs');
   return log.transports.file.getFile().clear();
 }
