67609b8c169fcb3f66aeb0d662df2d3e0f846f52
Shriram Giridhara <shrirambhavin@gmail.com>
2024-10-09 18:43:38 -0400

    Uncomment runInference logic

--- src/main/model-apps/isr-model/isr-model-service.ts
+++ src/main/model-apps/isr-model/isr-model-service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable class-methods-use-this */
-// import log from 'electron-log';
+import log from 'electron-log';
 import InferenceService, {
   ErrorResponse,
   InferenceArgs,
@@ -9,7 +9,6 @@ import InferenceService, {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const SERVER_HEALTH_SLUG = '/health';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const SERVER_INFERENCE_SLUG = '/run-inference';
 
 class ISRModelService implements InferenceService {
@@ -37,48 +36,39 @@ class ISRModelService implements InferenceService {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     signal: AbortSignal,
   ): Promise<SuccessResponse | ErrorResponse> {
-    // const { serverAddress, serverPort } = args.server;
-    // const body = {
-    //   inputDir: args.inputs[0].path,
-    //   outputDir: args.outputs[0].path,
-    //   parameters: args.parameters,
-    // };
-    // return fetch(
-    //   `http://${serverAddress}:${serverPort}${SERVER_INFERENCE_SLUG}`,
-    //   {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(body),
-    //     signal,
-    //   },
-    // )
-    //   .then((res) => {
-    //     if (res.status !== 200) {
-    //       log.error(`Job failed to execute. ${res}`);
-    //       return new ErrorResponse([{ message: res.statusText }]);
-    //     }
-    //     log.info(`Job has been completed. Here are the results:\n ${res}`);
-    //     return new SuccessResponse(res.json());
-    //   })
-    //   .catch((error) => {
-    //     if (error.name === 'AbortError') {
-    //       log.info('Job has been canceled.');
-    //       return new ErrorResponse([{ message: 'Job has been canceled.' }]);
-    //     }
-    //     log.error(`Job failed to execute. ${error}`);
-    //     return new ErrorResponse([{ message: error.message }]);
-    //   });
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(
-          new SuccessResponse({
-            output: 'SuperSmashingGreat!',
-          }),
-        );
-      }, 1000);
-    });
+    const { serverAddress, serverPort } = args.server;
+    const body = {
+      inputDir: args.inputs[0].path,
+      outputDir: args.outputs[0].path,
+      parameters: args.parameters,
+    };
+    return fetch(
+      `http://${serverAddress}:${serverPort}${SERVER_INFERENCE_SLUG}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+        signal,
+      },
+    )
+      .then((res) => {
+        if (res.status !== 200) {
+          log.error(`Job failed to execute. ${res}`);
+          return new ErrorResponse([{ message: res.statusText }]);
+        }
+        log.info(`Job has been completed. Here are the results:\n ${res}`);
+        return new SuccessResponse(res.json());
+      })
+      .catch((error) => {
+        if (error.name === 'AbortError') {
+          log.info('Job has been canceled.');
+          return new ErrorResponse([{ message: 'Job has been canceled.' }]);
+        }
+        log.error(`Job failed to execute. ${error}`);
+        return new ErrorResponse([{ message: error.message }]);
+      });
   }
 }
 
