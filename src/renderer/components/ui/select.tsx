a836f1d81c07878546aafe59131c61246ed46f1e
Shriram Giridhara <shrirambhavin@gmail.com>
2024-10-08 22:02:32 -0400

    Fix warning: Function components cannot be given refs.

--- src/renderer/components/ui/select.tsx
+++ src/renderer/components/ui/select.tsx
@@ -31,9 +31,7 @@ const SelectTrigger = React.forwardRef<
     {...props}
   >
     {children}
-    <SelectPrimitive.Icon asChild>
-      <CaretSortIcon className="h-4 w-4 opacity-50" />
-    </SelectPrimitive.Icon>
+    <CaretSortIcon className="h-4 w-4 opacity-50" />
   </SelectPrimitive.Trigger>
 ));
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
