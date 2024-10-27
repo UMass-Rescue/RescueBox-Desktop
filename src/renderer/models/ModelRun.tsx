b2ec10273dd851c4d5695bd4f6c8fc234c10cb17
Shriram Giridhara <shrirambhavin@gmail.com>
2024-10-26 21:23:16 -0400

    Write model run page

--- src/renderer/models/ModelRun.tsx
+++ src/renderer/models/ModelRun.tsx
@@ -1,9 +1,64 @@
-import { useParams } from 'react-router-dom';
+/* eslint-disable react/jsx-props-no-spreading */
+import LoadingScreen from '@shadcn/components/LoadingScreen';
+import { useApiRoutes } from '@shadcn/lib/hooks';
+import { cn } from '@shadcn/lib/utils';
+import { NavLink, useOutlet, useParams } from 'react-router-dom';
 
 function ModelRun() {
   const { modelUid } = useParams();
+  const outlet = useOutlet();
 
-  return <div>Model Run Page for model uid {modelUid}</div>;
+  const {
+    data: apiRoutes,
+    error: apiRoutesError,
+    isLoading: apiRoutesIsLoading,
+  } = useApiRoutes(modelUid);
+
+  if (!modelUid) {
+    return <div>Error: Model UID not found</div>;
+  }
+
+  if (!apiRoutes) {
+    return <LoadingScreen />;
+  }
+  if (apiRoutesIsLoading) {
+    return <LoadingScreen />;
+  }
+  if (apiRoutesError) {
+    return <div>Error loading api routes</div>;
+  }
+
+  return (
+    <div>
+      <div className="tabs mt-3 mx-2">
+        <div className="flex flex-row space-x-4">
+          {apiRoutes
+            .filter((apiRoute) => 'order' in apiRoute)
+            .sort((a, b) => a.order - b.order)
+            .map((apiRoute) => (
+              <NavLink
+                key={apiRoute.order}
+                to={`/models/${modelUid}/run${apiRoute.run_task}`}
+                className={({ isActive }) =>
+                  isActive
+                    ? cn(
+                        'text-md md:text-md lg:text-md xl:text-md',
+                        'text-blue-500 underline underline-offset-8 font-semibold',
+                      )
+                    : cn(
+                        'text-md md:text-md lg:text-md xl:text-md',
+                        'hover:text-blue-500 font-semibold',
+                      )
+                }
+              >
+                {apiRoute.short_title}
+              </NavLink>
+            ))}
+        </div>
+      </div>
+      <div>{outlet}</div>
+    </div>
+  );
 }
 
 export default ModelRun;
---
+++ src/renderer/models/ModelRunTask.tsx
@@ -0,0 +1,97 @@
+import GreenRunIcon from '@shadcn/components/icons/GreenRunIcon';
+import InputField from '@shadcn/components/InputField';
+import LoadingScreen from '@shadcn/components/LoadingScreen';
+import ParameterField from '@shadcn/components/ParameterField';
+import { Button } from '@shadcn/components/ui/button';
+import { useTaskSchema } from '@shadcn/lib/hooks';
+import { Controller, useForm } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import { InputSchema, ParameterSchema } from 'src/shared/generated_models';
+
+export default function ModelRunTask() {
+  const { modelUid, taskRoute } = useParams();
+
+  const {
+    data: taskSchema,
+    error: taskSchemaError,
+    isLoading: taskSchemaIsLoading,
+  } = useTaskSchema(modelUid, `/${taskRoute}`);
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isValid },
+  } = useForm({ mode: 'onChange' });
+
+  if (!taskSchema) {
+    return <LoadingScreen />;
+  }
+  if (taskSchemaIsLoading) {
+    return <LoadingScreen />;
+  }
+  if (taskSchemaError) {
+    return <div>Error loading task schema</div>;
+  }
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="mt-6 m-2">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="text-2xl font-extrabold mb-4">Select Inputs</h1>
+        <div className="grid grid-cols-1 gap-6">
+          {taskSchema.inputs.map((inputSchema: InputSchema) => (
+            <div key={inputSchema.key}>
+              <Controller
+                name={inputSchema.key}
+                control={control}
+                render={({ field }) => (
+                  <InputField
+                    inputSchema={inputSchema}
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
+                )}
+              />
+            </div>
+          ))}
+        </div>
+        <hr className="mt-8 mb-4" />
+        {taskSchema.parameters.length > 0 && (
+          <>
+            <h1 className="text-2xl font-extrabold mb-4">Select Parameters</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {taskSchema.parameters.map((parameterSchema: ParameterSchema) => (
+                <div key={parameterSchema.key}>
+                  <Controller
+                    name={parameterSchema.key}
+                    control={control}
+                    render={({ field }) => (
+                      <ParameterField
+                        parameterSchema={parameterSchema}
+                        value={field.value}
+                        onChange={field.onChange}
+                      />
+                    )}
+                  />
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+        <div className="bottom-5 left-0 w-full mt-5 py-4">
+          <Button
+            type="submit"
+            className="w-full flex flex-row gap-2 hover:-translate-y-0.5 transition-all py-2 px-6 rounded-lg bg-green-600 hover:bg-green-500"
+            disabled={!isValid}
+          >
+            Run Model
+            <GreenRunIcon />
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
