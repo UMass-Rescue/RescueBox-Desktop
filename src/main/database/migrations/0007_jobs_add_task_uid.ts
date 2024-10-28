040bf4efb1b795201ed6033f57bcae524d8332b5
Shreyan Mallik <shrnmallik@gmail.com>
2024-10-27 20:53:24 -0400

    added to migration7 - remove taskRoute from jobs table

--- src/main/database/migrations/0007_jobs_add_task_uid.ts
+++ src/main/database/migrations/0007_jobs_add_task_uid.ts
@@ -6,9 +6,11 @@ const MIGRATION_NAME = '0007_jobs_add_task_uid';
 const migration0007JobsAddTaskUid = {
   name: MIGRATION_NAME,
   async up({ context: queryInterface }: { context: QueryInterface }) {
+    await queryInterface.removeColumn(TABLE_NAME, 'taskRoute');
     await queryInterface.addColumn(TABLE_NAME, 'taskUid', DataTypes.STRING);
   },
   async down({ context: queryInterface }: { context: QueryInterface }) {
+    await queryInterface.addColumn(TABLE_NAME, 'taskRoute', DataTypes.STRING);
     await queryInterface.removeColumn(TABLE_NAME, 'taskUid');
   },
 };
