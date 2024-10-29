8e959c1696714b1aa04e581db2e050808fcf913f
Shriram Giridhara <shrirambhavin@gmail.com>
2024-10-29 15:54:18 -0400

    Add video preview component

--- src/renderer/jobs/JobViewOutputs.tsx
+++ src/renderer/jobs/JobViewOutputs.tsx
@@ -1,4 +1,6 @@
 import { useParams } from 'react-router-dom';
+import PreviewModal from '@shadcn/components/PreviewModal';
+import { FileResponse } from 'src/shared/generated_models';
 import { useJob } from '../lib/hooks';
 
 function JobViewOutputs() {
@@ -10,7 +12,23 @@ function JobViewOutputs() {
   if (jobError)
     return <div>failed to load job. Error: {jobError.toString()}</div>;
 
-  return <div>Job ID: {jobId}</div>;
+  // const { response } = job;
+  // if (!response) return <div>No response available</div>;
+
+  const response: FileResponse = {
+    output_type: 'file',
+    path: 'C:\\Users\\LENOVO\\Videos\\Captures\\super-res-demo.mp4',
+    file_type: 'video',
+    title: 'Super Resolution Demo',
+    subtitle: 'Demo Video for ISR Model',
+  };
+
+  return (
+    <div>
+      <div>Job ID: {jobId}</div>
+      <PreviewModal response={response} />
+    </div>
+  );
 }
 
 export default JobViewOutputs;
