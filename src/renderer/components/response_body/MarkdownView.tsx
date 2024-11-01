17d0abe3d2775f58d746065911575206a7a4892f
Shriram Giridhara <shrirambhavin@gmail.com>
2024-11-01 17:43:41 -0400

    Render markdown response and move video preview into previews folder

---
+++ src/renderer/components/response_body/MarkdownView.tsx
@@ -0,0 +1,22 @@
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeExternalLinks from 'rehype-external-links';
+import { MarkdownResponse } from 'src/shared/generated_models';
+
+export default function MarkdownView({
+  response,
+}: {
+  response: MarkdownResponse;
+}) {
+  return (
+    <div className="m-2 mr-4 prose max-w-full markdown">
+      <Markdown
+        rehypePlugins={[[rehypeExternalLinks, { target: '_blank' }]]}
+        className="text-black"
+        remarkPlugins={[remarkGfm]}
+      >
+        {`# ${response.title}\n${response.subtitle || ''}\n${response.value}`}
+      </Markdown>
+    </div>
+  );
+}
--- src/renderer/jobs/JobViewOutputs.tsx
+++ src/renderer/jobs/JobViewOutputs.tsx
@@ -1,7 +1,9 @@
 import { useParams } from 'react-router-dom';
 import PreviewModal from 'src/renderer/components/PreviewModal';
 import videoResponse from 'src/shared/dummy_data/file_response_video';
+import markdownResponseBody from 'src/shared/dummy_data/markdown_response';
 import { useJob } from '../lib/hooks';
+import MarkdownView from '../components/response_body/MarkdownView';
 
 function JobViewOutputs() {
   const { jobId } = useParams();
@@ -16,8 +18,8 @@ function JobViewOutputs() {
   if (!response) return <div>No response available</div>;
 
   return (
-    <div>
-      <div>Job ID: {jobId}</div>
+    <div className="border border-gray-300 rounded-lg mt-5 p-4 shadow-md bg-white">
+      <MarkdownView response={markdownResponseBody} />
       <PreviewModal response={videoResponse} />
     </div>
   );
