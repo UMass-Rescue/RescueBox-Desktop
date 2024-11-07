d8693fc19307711585a87d4c58d912249ac3b8f4
Shriram Giridhara <shrirambhavin@gmail.com>
2024-11-07 11:40:19 -0500

    Add log

--- src/renderer/navigation/NavigationListener.tsx
+++ src/renderer/navigation/NavigationListener.tsx
@@ -1,11 +1,17 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import log from 'electron-log/renderer';
 
 export default function NavigationListener() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    window.electronAPI?.onNavigate((page) => {
+    if (window.electronAPI === undefined) {
+      log.error('Electron API handler is not defined.');
+      return () => {};
+    }
+
+    window.electronAPI.onNavigate((page) => {
       navigate(page);
     });
 
