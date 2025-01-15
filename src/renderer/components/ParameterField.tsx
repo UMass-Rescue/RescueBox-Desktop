e17b0ed0728d19945a60787c4e49c314ca2703ae
Atharva Kale <atharvakale343@gmail.com>
2025-01-15 20:09:11 +0530

    add better compiler errors and fix loading screen

--- src/renderer/components/ParameterField.tsx
+++ src/renderer/components/ParameterField.tsx
@@ -1,11 +1,94 @@
 import { ParameterSchema } from 'src/shared/generated_models';
-import { match } from 'ts-pattern';
+import { match, NonExhaustiveError } from 'ts-pattern';
+import log from 'electron-log/renderer';
 import TextField from './parameter_fields/TextField';
 import IntField from './parameter_fields/IntField';
 import FloatField from './parameter_fields/FloatField';
 import EnumField from './parameter_fields/EnumField';
 import RangedIntField from './parameter_fields/RangedIntField';
 import RangedFloatField from './parameter_fields/RangedFloatField';
+
+function getFieldByParameterSchema(
+  parameterSchema: ParameterSchema,
+  value: any,
+  onChange: (value: any) => void,
+  disabled: boolean,
+) {
+  try {
+    return match(parameterSchema.value)
+      .with({ parameterType: 'text' }, () => {
+        return (
+          <TextField
+            parameterSchema={parameterSchema}
+            value={value}
+            onChange={onChange}
+            disabled={disabled}
+          />
+        );
+      })
+      .with({ parameterType: 'int' }, () => {
+        return (
+          <IntField
+            parameterSchema={parameterSchema}
+            value={value}
+            onChange={onChange}
+            disabled={disabled}
+          />
+        );
+      })
+      .with({ parameterType: 'float' }, () => {
+        return (
+          <FloatField
+            parameterSchema={parameterSchema}
+            value={value}
+            onChange={onChange}
+            disabled={disabled}
+          />
+        );
+      })
+      .with({ parameterType: 'enum' }, () => {
+        return (
+          <EnumField
+            parameterSchema={parameterSchema}
+            value={value}
+            onChange={onChange}
+            disabled={disabled}
+          />
+        );
+      })
+      .with({ parameterType: 'ranged_int' }, () => {
+        return (
+          <RangedIntField
+            parameterSchema={parameterSchema}
+            value={value}
+            onChange={onChange}
+            disabled={disabled}
+          />
+        );
+      })
+      .with({ parameterType: 'ranged_float' }, () => {
+        return (
+          <RangedFloatField
+            parameterSchema={parameterSchema}
+            value={value}
+            onChange={onChange}
+            disabled={disabled}
+          />
+        );
+      })
+      .exhaustive();
+  } catch (e) {
+    if (e instanceof NonExhaustiveError) {
+      log.error('Received an unexpected parameter schema.', parameterSchema);
+      return (
+        <div>
+          Unsupported parameter type: {parameterSchema.value.parameterType}
+        </div>
+      );
+    }
+    throw e;
+  }
+}
 
 export default function ParameterField({
   parameterSchema,
@@ -19,7 +102,6 @@ export default function ParameterField({
   // eslint-disable-next-line
   disabled?: boolean;
 }) {
-  const { value: paramValue } = parameterSchema;
   return (
     <div>
       <h2 className="font-semibold text-sm xl:text-base mb-2">
@@ -28,72 +110,7 @@ export default function ParameterField({
       {parameterSchema.subtitle && (
         <p className="text-xs mt-1">{parameterSchema.subtitle}</p>
       )}
-      {match(paramValue)
-        .with({ parameterType: 'text' }, () => {
-          return (
-            <TextField
-              parameterSchema={parameterSchema}
-              value={value}
-              onChange={onChange}
-              disabled={disabled}
-            />
-          );
-        })
-        .with({ parameterType: 'int' }, () => {
-          return (
-            <IntField
-              parameterSchema={parameterSchema}
-              value={value}
-              onChange={onChange}
-              disabled={disabled}
-            />
-          );
-        })
-        .with({ parameterType: 'float' }, () => {
-          return (
-            <FloatField
-              parameterSchema={parameterSchema}
-              value={value}
-              onChange={onChange}
-              disabled={disabled}
-            />
-          );
-        })
-        .with({ parameterType: 'enum' }, () => {
-          return (
-            <EnumField
-              parameterSchema={parameterSchema}
-              value={value}
-              onChange={onChange}
-              disabled={disabled}
-            />
-          );
-        })
-        .with({ parameterType: 'ranged_int' }, () => {
-          return (
-            <RangedIntField
-              parameterSchema={parameterSchema}
-              value={value}
-              onChange={onChange}
-              disabled={disabled}
-            />
-          );
-        })
-        .with({ parameterType: 'ranged_float' }, () => {
-          return (
-            <RangedFloatField
-              parameterSchema={parameterSchema}
-              value={value}
-              onChange={onChange}
-              disabled={disabled}
-            />
-          );
-        })
-        .otherwise(() => {
-          return (
-            <div>Unsupported parameter type: {paramValue.parameterType}</div>
-          );
-        })}
+      {getFieldByParameterSchema(parameterSchema, value, onChange, disabled)}
     </div>
   );
 }
--- src/renderer/jobs/PreviewResponseBody.tsx
+++ src/renderer/jobs/PreviewResponseBody.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ResponseBody } from 'src/shared/generated_models';
-import { match } from 'ts-pattern';
+import { match, NonExhaustiveError } from 'ts-pattern';
 import MarkdownView from '../components/response_body/text_views/MarkdownView';
 import DirectoryView from '../components/response_body/directory_views/DirectoryView';
 import BatchDirectoryView from '../components/response_body/directory_views/BatchDirectoryView';
@@ -14,29 +14,34 @@ export default function PreviewResponseBody({
 }: {
   response: ResponseBody;
 }) {
-  return match(response)
-    .with({ output_type: 'file' }, (fileResponse) => {
-      return <FileView response={fileResponse} />;
-    })
-    .with({ output_type: 'directory' }, (directoryResponse) => {
-      return <DirectoryView response={directoryResponse} />;
-    })
-    .with({ output_type: 'markdown' }, (markdownResponse) => {
-      return <MarkdownView response={markdownResponse} />;
-    })
-    .with({ output_type: 'text' }, (textResponse) => {
-      return <TextView response={textResponse} />;
-    })
-    .with({ output_type: 'batchfile' }, (batchFileResponse) => {
-      return <BatchFileView response={batchFileResponse} />;
-    })
-    .with({ output_type: 'batchtext' }, (batchTextResponse) => {
-      return <BatchTextView response={batchTextResponse} />;
-    })
-    .with({ output_type: 'batchdirectory' }, (batchDirectoryResponse) => {
-      return <BatchDirectoryView response={batchDirectoryResponse} />;
-    })
-    .otherwise(() => {
+  try {
+    return match(response)
+      .with({ output_type: 'file' }, (fileResponse) => {
+        return <FileView response={fileResponse} />;
+      })
+      .with({ output_type: 'directory' }, (directoryResponse) => {
+        return <DirectoryView response={directoryResponse} />;
+      })
+      .with({ output_type: 'markdown' }, (markdownResponse) => {
+        return <MarkdownView response={markdownResponse} />;
+      })
+      .with({ output_type: 'text' }, (textResponse) => {
+        return <TextView response={textResponse} />;
+      })
+      .with({ output_type: 'batchfile' }, (batchFileResponse) => {
+        return <BatchFileView response={batchFileResponse} />;
+      })
+      .with({ output_type: 'batchtext' }, (batchTextResponse) => {
+        return <BatchTextView response={batchTextResponse} />;
+      })
+      .with({ output_type: 'batchdirectory' }, (batchDirectoryResponse) => {
+        return <BatchDirectoryView response={batchDirectoryResponse} />;
+      })
+      .exhaustive();
+  } catch (e) {
+    if (e instanceof NonExhaustiveError) {
       return <div>Unknown Response</div>;
-    });
+    }
+    throw e;
+  }
 }
